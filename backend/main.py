d3e263cb39eef112d5a4969b3f87e2595ff4d2da
Aditya Singh <adityasingh3726as@gmail.com>
2026-09-01 23:29:52 +0000

    Update CORS to allow all origins

--- backend/main.py
+++ backend/main.py
@@ -17,11 +17,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "https://der-01-disaster-map.vercel.app",
-        "http://localhost:5500",
-        "http://127.0.0.1:5500",
-    ],
+    allow_origins=[*],
     allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
